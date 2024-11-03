d314bed4b9bf8b0554a312534fae209d92b1b2bd
KrutaRyba <n.u.leshchova@gmail.com>
2024-11-03 12:49:55 +0100

    Update TourMasterDataLoading.sql

--- SQL schemas/TourMasterDataLoading.sql
+++ SQL schemas/TourMasterDataLoading.sql
@@ -1,23 +1,23 @@
 USE TourMaster;
 GO
 
-BULK INSERT Client FROM 'D:\Data\Clients.bulk' WITH (FIELDTERMINATOR='|');
+BULK INSERT Client FROM 'D:\Data\Clients.bulk' WITH (FIELDTERMINATOR = '|', ROWTERMINATOR = '0x0A');
 GO
 
-BULK INSERT Hotel FROM 'D:\Data\Hotels.bulk' WITH (FIELDTERMINATOR='|');
+BULK INSERT Hotel FROM 'D:\Data\Hotels.bulk' WITH (FIELDTERMINATOR = '|', ROWTERMINATOR = '0x0A');
 GO
 
-BULK INSERT Transporter FROM 'D:\Data\Transporters.bulk' WITH (FIELDTERMINATOR='|');
+BULK INSERT Transporter FROM 'D:\Data\Transporters.bulk' WITH (FIELDTERMINATOR = '|', ROWTERMINATOR = '0x0A');
 GO
 
-BULK INSERT Tour FROM 'D:\Data\Tours.bulk' WITH (FIELDTERMINATOR='|');
+BULK INSERT Tour FROM 'D:\Data\Tours.bulk' WITH (FIELDTERMINATOR = '|', ROWTERMINATOR = '0x0A');
 GO
 
-BULK INSERT RoomReservation FROM 'D:\Data\Reservations.bulk' WITH (FIELDTERMINATOR='|');
+BULK INSERT RoomReservation FROM 'D:\Data\Reservations.bulk' WITH (FIELDTERMINATOR = '|', ROWTERMINATOR = '0x0A');
 GO
 
-BULK INSERT Transportation FROM 'D:\Data\Transportations.bulk' WITH (FIELDTERMINATOR='|');
+BULK INSERT Transportation FROM 'D:\Data\Transportations.bulk' WITH (FIELDTERMINATOR = '|', ROWTERMINATOR = '0x0A');
 GO
 
-BULK INSERT Tourist FROM 'D:\Data\Tourists.bulk' WITH (FIELDTERMINATOR='|');
+BULK INSERT Tourist FROM 'D:\Data\Tourists.bulk' WITH (FIELDTERMINATOR = '|', ROWTERMINATOR = '0x0A');
 GO
